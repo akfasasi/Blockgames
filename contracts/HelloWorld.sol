d5edca6146a805e2c384751200877581d95124cf
"A.K. Fasasi" <"akfasasi@outlook.com">
2022-03-09 13:34:36 +0100

    My commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: CPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+
+    uint256 btc;
+    int256 eth;
+
+// We can receive Bitcoin for storage thrpugh this function
+    function storeBTC(uint256 _btc) public {
+        btc = _btc;
+    }
+
+// We can receive Ether for storage using this function
+    function storeETH(int _eth) external {
+        eth = _eth;
+    }
+
+// Here we can view what we have in our BTC wallet
+    function retrieveBTC() public view returns (uint256) {
+        return btc;
+    }
+
+// Here we can view what we have in our ETH wallet
+    function retrieveETH() external view returns (int) {
+        return eth;
+    }
+}
